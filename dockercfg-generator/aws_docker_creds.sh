bda162f423292eb0bb0eb9e393a5a5916f15bc9a
Joe Siewert <jsiewert@cloudbees.com>
2021-05-11 12:40:24 -0500

    Update comments

--- dockercfg-generator/aws_docker_creds.sh
+++ dockercfg-generator/aws_docker_creds.sh
@@ -38,9 +38,9 @@ else
 fi
 
 # fetching aws docker login
-echo "Logging into AWS ECR with Account ${AWS_ACCOUNT}"
-# AWS has deprecated the get-login function in favor of get-login-password
+# aws deprecated the get-login function in favor of get-login-password
 # https://docs.aws.amazon.com/cli/latest/reference/ecr/get-login.html
+echo "Logging into AWS ECR with account ${AWS_ACCOUNT}"
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 # writing aws docker creds to desired path
